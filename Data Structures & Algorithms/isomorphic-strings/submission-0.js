36b2758ba26a15e8b47743bf06dbbe0ebd443bf1
Naima e Bogran <Naima.E.Bogran@gmail.com>
2026-04-24 11:11:29 -0400

    Add: isomorphic-strings - submission-0

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-0.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    helper(s, t) {
+        const map = new Map();
+        for (let i = 0; i < s.length; i++) {
+            if (map.has(s[i]) && map.get(s[i]) !== t[i]) {
+                return false;
+            }
+            map.set(s[i], t[i]);
+        }
+        return true;
+    }
+
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isIsomorphic(s, t) {
+        return this.helper(s, t) && this.helper(t, s);
+    }
+}
